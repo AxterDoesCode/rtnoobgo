8e5caf7d7d4ae485a67cbaad4eebba90184faf73
AxterDoesCode <chau.alex88@hotmail.com>
2023-10-19 14:54:58 +0100

    Finally starting to work

--- cmd/rt/main.go
+++ cmd/rt/main.go
@@ -8,15 +8,9 @@ import (
 )
 
 func main() {
-	O := vec3.New(0, 0, 0)
+	Origin := vec3.New(0, 0, 0)
 	scene := Scene{
 		Spheres: []*Sphere{},
-	}
-
-	s1 := &Sphere{
-		center: vec3.New(0, -1, 3),
-		radius: 1,
-		color:  canvas.NewColor(255, 0, 0),
 	}
 
 	s2 := &Sphere{
@@ -31,15 +25,21 @@ func main() {
 		color:  canvas.NewColor(0, 255, 0),
 	}
 
+	s1 := &Sphere{
+		center: vec3.New(0, 1, 5),
+		radius: 1,
+		color:  canvas.NewColor(255, 0, 0),
+	}
+
+    scene.AddSphere(s1)
 	scene.AddSphere(s2)
-	scene.AddSphere(s1)
 	scene.AddSphere(s3)
 
 	canvas := canvas.NewCanvas(256, 256)
 	for x := -canvas.Width / 2; x <= canvas.Width/2; x++ {
 		for y := -canvas.Height / 2; y <= canvas.Height/2; y++ {
 			D := CanvasToViewport(x, y, canvas.Width, canvas.Height, 1, 1)
-			color := TraceRay(O, D, 1, math.Inf(1), scene)
+			color := TraceRay(Origin, D, 1, math.Inf(1), scene)
 			canvas.PutPixel(x, y, color)
 		}
 	}
--- cmd/rt/utility.go
+++ cmd/rt/utility.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-    "math"
-    "github.com/AxterDoesCode/rtnoobgo/pkg/vec3"
-    "github.com/AxterDoesCode/rtnoobgo/pkg/canvas"
+	"math"
+
+	"github.com/AxterDoesCode/rtnoobgo/pkg/canvas"
+	"github.com/AxterDoesCode/rtnoobgo/pkg/vec3"
 )
 
 type Scene struct {
@@ -13,54 +14,54 @@ type Scene struct {
 type Sphere struct {
 	center vec3.Vec3
 	radius int
-	color canvas.Color
+	color  canvas.Color
 }
 
-func (s *Scene) AddSphere (sphere *Sphere) {
-    s.Spheres = append(s.Spheres, sphere)
+func (s *Scene) AddSphere(sphere *Sphere) {
+	s.Spheres = append(s.Spheres, sphere)
 }
 
 func CanvasToViewport(x, y, cw, ch int, vw, vh float64) vec3.Vec3 {
-    var d float64 = 1
+	var d float64 = 1
 	return vec3.Vec3{X: float64(x) * (vw / float64(cw)), Y: float64(y) * (vh / float64(ch)), Z: d}
 }
 
-func IntersectRaySphere (O, D vec3.Vec3, sphere Sphere) (float64, float64) {
-    r := sphere.radius
-    CO := vec3.Add(O, D.Neg())
+func IntersectRaySphere(O, D vec3.Vec3, sphere Sphere) (float64, float64) {
+	r := sphere.radius
+	CO := vec3.Add(O, sphere.center.Neg())
 
-    a := vec3.Dot(D, D)
-    b := 2 * vec3.Dot(CO, D)
-    c := vec3.Dot(CO, CO) - float64(r*r)
+	a := vec3.Dot(D, D)
+	b := 2 * vec3.Dot(CO, D)
+	c := vec3.Dot(CO, CO) - float64(r*r)
 
-    discriminant := (b*b) - (4*a*c)
-    if discriminant < 0 {
-        return math.Inf(1), math.Inf(1)
-    }
+	discriminant := (b * b) - (4 * a * c)
+	if discriminant < 0 {
+		return math.Inf(1), math.Inf(1)
+	}
 
-    t1 := (-b + math.Sqrt(discriminant)) / (2*a)
-    t2 := (-b - math.Sqrt(discriminant)) / (2*a)
-    return t1, t2
+	t1 := (-b + math.Sqrt(discriminant)) / (2 * a)
+	t2 := (-b - math.Sqrt(discriminant)) / (2 * a)
+	return t1, t2
 }
 
 func TraceRay(Origin, D vec3.Vec3, t_min, t_max float64, scene Scene) canvas.Color {
 	closest_t := math.Inf(1)
-    var closest_sphere *Sphere
+	var closest_sphere *Sphere
 
 	for _, sphere := range scene.Spheres {
 		t1, t2 := IntersectRaySphere(Origin, D, *sphere)
-        if t1 >= t_min && t1 <= t_max && t1 < closest_t {
-            closest_t = t1
-            closest_sphere = sphere
-        }
+		if t1 >= t_min && t1 <= t_max && t1 < closest_t {
+			closest_t = t1
+			closest_sphere = sphere
+		}
 
-        if t2 >= t_min && t2 <= t_max && t2 < closest_t {
-            closest_t = t2
-            closest_sphere = sphere
-        }
+		if t2 >= t_min && t2 <= t_max && t2 < closest_t {
+			closest_t = t2
+			closest_sphere = sphere
+		}
 	}
-    if closest_sphere == nil {
-        return canvas.NewColor(0, 0, 0)
-    }
-    return closest_sphere.color
+	if closest_sphere == nil {
+		return canvas.NewColor(0, 0, 0)
+	}
+	return closest_sphere.color
 }
